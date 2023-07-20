cc480e22ef0625669f65fe536c6a93f3255db546
Anastasiya M <mega.gara11@gmail.com>
2023-07-20 12:49:34 +0700

    add pom.xml

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,24 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxTwo() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {1000000000, 15, 18, 3, 7, 190, 2594, 6758, 34567, 100, 1000000000};
+        long expected = 1000000000;
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxThree() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {0, 134, 3, 8, 50, 5, 25, 96, 10, 4, 0};
+        long expected = 134;
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
